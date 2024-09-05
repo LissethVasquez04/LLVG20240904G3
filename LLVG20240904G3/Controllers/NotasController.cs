664b5fa885ed7a6df6dd85c8da1d7379121caf06
LissethVasquez04 <vasquezlisseth004@gmail.com>
2024-09-04 20:40:46 -0600

    Controlador de Nota creado

---
+++ LLVG20240904G3/Controllers/NotasController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LLVG20240904G3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotasController : ControllerBase
+    {
+        // GET: api/<NotasController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<NotasController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<NotasController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<NotasController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<NotasController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
