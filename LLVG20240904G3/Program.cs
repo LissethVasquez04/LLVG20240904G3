8f657b7eda0c295d4f2ab6ef6e4ac664ae1ee13e
LissethVasquez04 <vasquezlisseth004@gmail.com>
2024-09-04 19:21:41 -0600

    Program terminado

--- LLVG20240904G3/Program.cs
+++ LLVG20240904G3/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,32 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        // Configura el nombre del parámetro de URL para redireccionamiento no autorizado
+        options.ReturnUrlParameter = "unauthorized";
+        options.Events = new CookieAuthenticationEvents
+        {
+            OnRedirectToLogin = context =>
+            {
+                // Cambia el código de estado a No autorizado
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                // Establece el tipo de contenido como JSON (u otro formato deseado)
+                context.Response.ContentType = "application/json";
+                var message = new
+                {
+                    error = "No autorizado",
+                    message = "Debe iniciar sesión para acceder a este recurso."
+                };
+                // Serializa el objeto 'message' en formato JSON
+                var jsonMessage = JsonSerializer.Serialize(message);
+                // Escribe el mensaje JSON en la respuesta HTTP
+                return context.Response.WriteAsync(jsonMessage);
+            }
+        };
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
